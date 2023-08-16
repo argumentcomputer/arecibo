f501e15861a3c70f8c2a04f6a66a0cf6616cff9d
emmorais <eduardo.morais@gmail.com>
2023-08-16 11:41:43 -0300

    Small subtasks: params, phi, un function, EE
    Minor adjustments

--- src/provider/zeromorph.rs
+++ src/provider/zeromorph.rs
@@ -1,17 +1,47 @@
 // ZeroMorph
 
-use crate::provider::kzg::KZGParams;
-use crate::provider::kzg::KZGScheme;
+use crate::provider::kzg::{KZGParams, KZGScheme};
+use crate::Group;
 use ff::Field;
+use halo2curves::bn256::G2Affine;
 use halo2curves::bn256::{Fr, G1Affine, G1, G2};
+use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
 use std::ops::Mul;
+
+/// Provides an implementation of a polynomial evaluation engine using ZeroMorph
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct EvaluationEngine<G: Group> {
+  _p: PhantomData<G>,
+}
+
+#[allow(dead_code)]
+fn phi(monomial: usize, n: u32) -> Vec<Fr> {
+  let pow2n = usize::pow(2, n);
+  let mut res = Vec::with_capacity(pow2n);
+  let mut i = monomial;
+  while i < pow2n {
+    res[i] = Fr::ONE;
+    i += monomial;
+  }
+  res
+}
+
+// Univariatization
+#[allow(dead_code)]
+fn un(a: &[Fr], _n: usize) -> &[Fr] {
+  // For all integers x in the domain [0, 2ˆn - 1]
+  // evaluate a at x and set coefficient at position x
+  a
+}
 
 #[derive(Clone)]
 pub struct ZeroMorphParams {
   pub nmax: u64,
   pub gen1: G1,
   pub gen2: G2,
-  pub powers_of_tau: Vec<G1Affine>,
+  pub powers_of_tau1: Vec<G1Affine>,
+  pub powers_of_tau2: Vec<G2Affine>,
   pub xi1: G1,
   pub xi2: G2,
   pub tau2: G2,
@@ -45,15 +75,17 @@ impl ZeroMorphScheme for ZeroMorphParams {
 
     let gen1: G1 = G1::generator();
     let gen2: G2 = G2::generator();
-    let mut powers = Vec::new();
+    let mut powers1 = Vec::new();
+    let mut powers2 = Vec::new();
     let mut xi1 = gen1;
     let mut xi2 = gen2;
     let mut tau2 = gen2;
 
     for i in 0..nmax {
-      let gtau = gen1;
-      let result = gtau.mul(tau.pow([i as u64]));
-      powers.push(result.into());
+      let gtau1 = gen1;
+      let gtau2 = gen2;
+      powers1.push(gtau1.mul(tau.pow([i as u64])).into());
+      powers2.push(gtau2.mul(tau.pow([i as u64])).into());
       xi1 = gen1.mul(xi);
       xi2 = gen2.mul(xi);
       tau2 = gen2.mul(tau);
@@ -62,7 +94,8 @@ impl ZeroMorphScheme for ZeroMorphParams {
       nmax,
       gen2: gen2.into(),
       gen1: gen1.into(),
-      powers_of_tau: powers,
+      powers_of_tau1: powers1,
+      powers_of_tau2: powers2,
       xi1,
       xi2,
       tau2,
