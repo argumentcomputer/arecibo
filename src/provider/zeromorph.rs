c3d027ea5c275310a033a290e3143170ba43e080
emmorais <eduardo.morais@gmail.com>
2023-08-16 18:31:03 -0300

    Minor adjustments

--- src/provider/zeromorph.rs
+++ src/provider/zeromorph.rs
@@ -1,14 +1,13 @@
 // ZeroMorph
 
 use crate::provider::kzg::{KZGParams, KZGScheme};
-use ff::Field;
-use halo2curves::bn256::{Fr, G1Affine, G1, G2};
-use std::ops::Mul;
-use serde::{Deserialize, Serialize};
 use crate::Group;
+use ff::Field;
+use halo2curves::bn256::G2Affine;
+use halo2curves::bn256::{Fr, G1Affine, G1, G2};
+use serde::{Deserialize, Serialize};
 use std::marker::PhantomData;
- use halo2curves::bn256::G2Affine;
-
+use std::ops::Mul;
 
 /// Provides an implementation of a polynomial evaluation engine using ZeroMorph
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -16,25 +15,25 @@ pub struct EvaluationEngine<G: Group> {
   _p: PhantomData<G>,
 }
 
-fn phi(monomial: usize, n: u32) -> &'static [Fr] {
-    let pow2n = usize::pow(2, n);
-    let mut res = vec!(Fr::from(0); pow2n);
-    let mut i = monomial;
-    while i < pow2n {
-        res[i] = Fr::ONE;
-        i += monomial;
-    };
-    &res[..]
+#[allow(dead_code)]
+fn phi(monomial: usize, n: u32) -> Vec<Fr> {
+  let pow2n = usize::pow(2, n);
+  let mut res = Vec::with_capacity(pow2n);
+  let mut i = monomial;
+  while i < pow2n {
+    res[i] = Fr::ONE;
+    i += monomial;
+  }
+  res
 }
 
 // Univariatization
-fn un(a: &[Fr], n: usize) -> &[Fr] {
-    // For all integers x in the domain [0, 2ˆn - 1]
-    // evaluate a at x and set coefficient at position x
-    a
+#[allow(dead_code)]
+fn un(a: &[Fr], _n: usize) -> &[Fr] {
+  // For all integers x in the domain [0, 2ˆn - 1]
+  // evaluate a at x and set coefficient at position x
+  a
 }
-
-
 
 #[derive(Clone)]
 pub struct ZeroMorphParams {
@@ -65,46 +64,46 @@ pub trait ZeroMorphScheme {
 }
 
 impl ZeroMorphScheme for ZeroMorphParams {
-    fn setup(nmax: u64) -> ZeroMorphParams {
-        let kzg = KZGParams::setup(nmax);
+  fn setup(nmax: u64) -> ZeroMorphParams {
+    let kzg = KZGParams::setup(nmax);
 
-        // FIXME: avoid repeated code
-        let rng = &mut rand::thread_rng();
-        let tau = Fr::random(rng);
-        let rng = &mut rand::thread_rng();
-        let xi = Fr::random(rng);
+    // FIXME: avoid repeated code
+    let rng = &mut rand::thread_rng();
+    let tau = Fr::random(rng);
+    let rng = &mut rand::thread_rng();
+    let xi = Fr::random(rng);
 
-        let gen1: G1 = G1::generator();
-        let gen2: G2 = G2::generator();
-        let mut powers1 = Vec::new();
-        let mut powers2 = Vec::new();
-        let mut xi1 = gen1;
-        let mut xi2 = gen2;
-        let mut tau2 = gen2;
+    let gen1: G1 = G1::generator();
+    let gen2: G2 = G2::generator();
+    let mut powers1 = Vec::new();
+    let mut powers2 = Vec::new();
+    let mut xi1 = gen1;
+    let mut xi2 = gen2;
+    let mut tau2 = gen2;
 
-        for i in 0..nmax {
-            let gtau1 = gen1;
-            let gtau2 = gen2;
-            powers1.push(gtau1.mul(tau.pow([i as u64])).into());
-            powers2.push(gtau2.mul(tau.pow([i as u64])).into());
-            xi1 = gen1.mul(xi);
-            xi2 = gen2.mul(xi);
-            tau2 = gen2.mul(tau);
-        }
-        ZeroMorphParams {
-            nmax,
-            gen2: gen2.into(),
-            gen1: gen1.into(),
-            powers_of_tau1: powers1,
-            powers_of_tau2: powers2,
-            xi1,
-            xi2,
-            tau2,
-            kzg,
-        }
+    for i in 0..nmax {
+      let gtau1 = gen1;
+      let gtau2 = gen2;
+      powers1.push(gtau1.mul(tau.pow([i as u64])).into());
+      powers2.push(gtau2.mul(tau.pow([i as u64])).into());
+      xi1 = gen1.mul(xi);
+      xi2 = gen2.mul(xi);
+      tau2 = gen2.mul(tau);
     }
+    ZeroMorphParams {
+      nmax,
+      gen2: gen2.into(),
+      gen1: gen1.into(),
+      powers_of_tau1: powers1,
+      powers_of_tau2: powers2,
+      xi1,
+      xi2,
+      tau2,
+      kzg,
+    }
+  }
 
-    /// Commit to univariate polynomial p given in coefficient representation
+  /// Commit to univariate polynomial p given in coefficient representation
   fn commit(&self, p: &[Fr]) -> (G1, Fr) {
     self.kzg.commit(p)
   }
