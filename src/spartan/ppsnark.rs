0690f646efc61045a71513f7e1cad240b258ba70
François Garillot <francois@garillot.net>
2023-08-16 20:30:18 -0400

    doc: Improve code comments by adding backticks for formatting
    - Back-ticks have been added around relevant technical terms, type names, and code references within the comments to better highlight them while reading.

--- src/spartan/ppsnark.rs
+++ src/spartan/ppsnark.rs
@@ -1,4 +1,4 @@
-//! This module implements RelaxedR1CSSNARK traits using a spark-based approach to prove evaluations of
+//! This module implements `RelaxedR1CSSNARK` traits using a spark-based approach to prove evaluations of
 //! sparse multilinear polynomials involved in Spartan's sum-check protocol, thereby providing a preprocessing SNARK
 //! The verifier in this preprocessing SNARK maintains a commitment to R1CS matrices. This is beneficial when using a
 //! polynomial commitment scheme in which the verifier's costs is succinct.
@@ -55,7 +55,7 @@ impl<Scalar: PrimeField> IdentityPolynomial<Scalar> {
   }
 }
 
-/// A type that holds R1CSShape in a form amenable to memory checking
+/// A type that holds `R1CSShape` in a form amenable to memory checking
 #[derive(Clone, Serialize, Deserialize, Abomonation)]
 #[serde(bound = "")]
 #[abomonation_bounds(where <G::Scalar as ff::PrimeField>::Repr: Abomonation)]
@@ -125,7 +125,7 @@ impl<G: Group> TranscriptReprTrait<G> for R1CSShapeSparkCommitment<G> {
 }
 
 impl<G: Group> R1CSShapeSparkRepr<G> {
-  /// represents R1CSShape in a Spark-friendly format amenable to memory checking
+  /// represents `R1CSShape` in a Spark-friendly format amenable to memory checking
   pub fn new(S: &R1CSShape<G>) -> R1CSShapeSparkRepr<G> {
     let N = {
       let total_nz = S.A.len() + S.B.len() + S.C.len();
@@ -891,7 +891,7 @@ where
     Ok((pk, vk))
   }
 
-  /// produces a succinct proof of satisfiability of a RelaxedR1CS instance
+  /// produces a succinct proof of satisfiability of a `RelaxedR1CS` instance
   fn prove(
     ck: &CommitmentKey<G>,
     pk: &Self::ProverKey,
@@ -1501,7 +1501,7 @@ where
     })
   }
 
-  /// verifies a proof of satisfiability of a RelaxedR1CS instance
+  /// verifies a proof of satisfiability of a `RelaxedR1CS` instance
   fn verify(&self, vk: &Self::VerifierKey, U: &RelaxedR1CSInstance<G>) -> Result<(), NovaError> {
     let mut transcript = G::TE::new(b"RelaxedR1CSSNARK");
     let mut u_vec: Vec<PolyEvalInstance<G>> = Vec::new();
