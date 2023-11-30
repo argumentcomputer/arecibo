f53c55babc83613a7a049bbc80ac7d387a8adf96
François Garillot <francois@garillot.net>
2023-11-30 17:50:21 -0500

    feat: Introduce new utility macros in spartan module
    - Introduced a macro `zip_with` in `src/spartan/mod.rs` to simplify the process of zipping multiple iterators and applying a single parallel function.
    - Developed additional macros `nested_tuple` and `nested_idents` for detailed nested tuple patterns and identifiers respectively.
    - Implemented `zip_all` macro that acts as a fold-right zipping operator for all given expressions.

--- src/spartan/mod.rs
+++ src/spartan/mod.rs
@@ -5,6 +5,67 @@
 //! We also provide direct.rs that allows proving a step circuit directly with either of the two SNARKs.
 //!
 //! In polynomial.rs we also provide foundational types and functions for manipulating multilinear polynomials.
+
+/// A macro to give syntactic sugar for zipWith pattern
+///
+/// ```rust
+/// use nova_snark::zip_with;
+/// use itertools::Itertools as _; // we use zip_eq to zip!
+/// let v = vec![0, 1, 2];
+/// let w = vec![2, 3, 4];
+/// let y = vec![4, 5, 6];
+/// 
+/// // Using the `zip_with!` macro to zip three iterators together and apply a closure
+/// // that sums the elements of each iterator.
+/// let res = zip_with!((v.iter(), w.iter(), y.iter()), |a, b, c| a + b + c)
+///     .collect::<Vec<_>>();
+///
+/// println!("{:?}", res); // Output: [6, 9, 12]
+/// ```
+macro_rules! zip_with {
+    (($e:expr $(, $rest:expr)*), $($move:ident)? |$($i:ident),+ $(,)?| $($work:tt)*) => {{
+      zip_all!(($e $(, $rest)*))
+            .map($($move)? |nested_idents!($($i),+)| {
+                $($work)*
+            })
+    }};
+}
+
+// Fold-right like nesting pattern for expressions a, b, c, d => (a, (b, (c, d)))
+#[doc(hidden)]
+#[allow(unused_macros)]
+macro_rules! nested_tuple {
+    ($a:expr, $b:expr) => {
+        ($a, $b)
+    };
+    ($first:expr, $($rest:expr),+) => {
+        ($first, nested_tuple!($($rest),+))
+    };
+}
+
+// Same as the above for idents
+#[doc(hidden)]
+#[allow(unused_macro_rules)]
+macro_rules! nested_idents {
+    ($a:ident, $b:ident) => {
+        ($a, $b)
+    };
+    ($first:ident, $($rest:ident),+) => {
+        ($first, nested_idents!($($rest),+))
+    };
+}
+
+// Fold-right like zipping
+#[doc(hidden)]
+macro_rules! zip_all {
+    (($e:expr,)) => {
+        $e
+    };
+    (($first:expr, $second:expr $(, $rest:expr)*)) => {
+        ($first.zip_eq(zip_all!(($second, $( $rest),*))))
+    };
+}
+
 pub mod batched;
 pub mod batched_ppsnark;
 pub mod direct;
