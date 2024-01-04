ff30d9b16b1bcb99ac27c84db6376ef364dfb005
François Garillot <4142+huitseeker@users.noreply.github.com>
2024-01-04 07:45:52 -0500

    chore: Refactors CPU MSM operations using halo2curves library (#230)
    - Expanded the `msm` module within the `provider/util` directory and introduced a new function, `cpu_best_msm`.
    - Changed the curves library dependency in `bn256_grumpkin.rs` from `pasta_curves` to `halo2curves`,
    - Removed the `msm.rs` file along with two associated functions `cpu_msm_serial` and `cpu_best_msm` used for non-GPU accelerated operations, and all related tests.
    - Reorganized the import of `CurveAffine, CurveExt` from the `halo2curves` library in `provider/mod.rs`.
    
    Fixes #193

--- src/provider/util/mod.rs
+++ src/provider/util/mod.rs
@@ -1,3 +1,12 @@
 /// Utilities for provider module
 pub(in crate::provider) mod fb_msm;
-pub(in crate::provider) mod msm;
+pub mod msm {
+  use halo2curves::msm::best_multiexp;
+  use halo2curves::CurveAffine;
+
+  // this argument swap is useful until Rust gets named arguments
+  // and saves significant complexity in macro code
+  pub fn cpu_best_msm<C: CurveAffine>(bases: &[C], scalars: &[C::Scalar]) -> C::Curve {
+    best_multiexp(scalars, bases)
+  }
+}
