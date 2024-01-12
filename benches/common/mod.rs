bef8333628b4599498fc4a2aeb758f23b9c6bb15
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2024-01-12 12:16:30 -0500

    Benchmark Pasta curves

--- benches/common/mod.rs
+++ benches/common/mod.rs
@@ -16,7 +16,7 @@ impl BenchParams {
     match output_type.as_ref() {
       "pr-comment" => BenchmarkId::new(name, format!("StepCircuitSize-{}", self.step_size)),
       "commit-comment" => BenchmarkId::new(
-        format!("ref={}", self.sha),
+        "Pasta",
         format!("{}-StepCircuitSize-{}", name, self.step_size),
       ),
       // TODO: refine "gh-pages"
