b5592720989710ee7707bb6d689bd549ffdc86e9
Matej Penciak <matej.penciak@gmail.com>
2024-02-15 16:15:32 -0500

    make `NUM_FE_WITHOUT_IO_FOR_CRHF` dependent on `NIO_NOVA_FOLD` and `BN_N_LIMBS`

--- src/constants.rs
+++ src/constants.rs
@@ -3,7 +3,7 @@
 pub(crate) const NUM_CHALLENGE_BITS: usize = 128;
 pub(crate) const BN_LIMB_WIDTH: usize = 64;
 pub(crate) const BN_N_LIMBS: usize = 4;
-pub(crate) const NUM_FE_WITHOUT_IO_FOR_CRHF: usize = 17;
+pub(crate) const NUM_FE_WITHOUT_IO_FOR_CRHF: usize = 9 + NIO_NOVA_FOLD * BN_N_LIMBS;
 pub(crate) const NUM_FE_FOR_RO: usize = 9;
 pub(crate) const NIO_NOVA_FOLD: usize = 2;
 
