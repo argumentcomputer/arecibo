f6439aea5e1082b5273c51de42c00479a4d603de
François Garillot <francois@garillot.net>
2024-02-28 15:49:31 -0500

    refactor: restrict visibility of nonnative

--- src/gadgets/mod.rs
+++ src/gadgets/mod.rs
@@ -1,5 +1,6 @@
 //! This module implements various gadgets necessary for Nova and applications built with Nova.
 pub mod ecc;
-pub(crate) mod nonnative;
+mod nonnative;
+pub use nonnative::{bignat::nat_to_limbs, util::f_to_nat};
 pub(crate) mod r1cs;
 pub(crate) mod utils;
