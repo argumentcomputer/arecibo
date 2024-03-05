07f571e11f346ef51d4c4ce3cf14ab0360982a30
Arthur Paulino <arthurleonardo.ap@gmail.com>
2024-03-05 09:37:33 -0300

    fix: typos (#357)

--- src/provider/non_hiding_zeromorph.rs
+++ src/provider/non_hiding_zeromorph.rs
@@ -85,7 +85,7 @@ where
   }
 
   /// Generate a commitment for a polynomial
-  /// Note that the scheme is not hidding
+  /// Note that the scheme is not hiding
   pub fn commit(
     prover_param: impl Borrow<KZGProverKey<E>>,
     poly: &UVKZGPoly<E::Fr>,
