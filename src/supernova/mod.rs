61334546b02839ce17e232c827e8cdcbea8816b2
Wyatt Benno <wyattbenno@gmail.com>
2024-03-14 13:46:02 -0500

    add small util for SuperNova testing (easy) (#358)
    * add small util for SuperNova testing
    
    * added another function for secondary
    
    * chore: document added method
    
    ---------
    
    Co-authored-by: Wyatt <wyatt@icme.io>
    Co-authored-by: François Garillot <francois@garillot.net>

--- src/supernova/mod.rs
+++ src/supernova/mod.rs
@@ -427,6 +427,22 @@ where
       .expect("Failure in retrieving digest")
   }
 
+  /// Returns the number of constraints and variables of inner circuit based on index
+  pub fn num_constraints_and_variables(&self, index: usize) -> (usize, usize) {
+    (
+      self.circuit_shapes[index].r1cs_shape.num_cons,
+      self.circuit_shapes[index].r1cs_shape.num_vars,
+    )
+  }
+
+  /// Returns the number of constraints and variables of the secondary circuit
+  pub fn num_constraints_and_variables_secondary(&self) -> (usize, usize) {
+    (
+      self.circuit_shape_secondary.r1cs_shape.num_cons,
+      self.circuit_shape_secondary.r1cs_shape.num_vars,
+    )
+  }
+
   /// All of the primary circuit digests of this [`PublicParams`]
   pub fn circuit_param_digests(&self) -> CircuitDigests<E1> {
     let digests = self
